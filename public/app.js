136337818f3c1ee1087c5abb15bb344ee29efd6a
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-04-08 14:14:55 +0100

    modules

--- public/app.js
+++ public/app.js
@@ -1,1 +1,19 @@
 "use strict";
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
+const form = document.querySelector('.new-item-form');
+// inputs
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault;
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
