76fdf13930724603ff388fdbf7140a8cdaa92e0e
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-04-08 15:10:23 +0100

    interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,44 @@
+interface isPerson {
+    name: string;
+    age: number;
+
+    speak(a: string):void;
+    spend(a: number):number;
+}
+
+const me: isPerson = {
+    name: 'shun',
+    age: 30,
+    speak(text: string):void {
+        console.log(text);
+    },
+    spend(amount: number):number {
+        console.log(' I spent', amount);
+        return amount
+    }
+}
+
+const greetPerson = (person: isPerson) => {
+    console.log('hello', person.name)
+}
+
+greetPerson(me)
+
+let someone: isPerson;
+
+
+console.log(me);
+
+
+
+
+
+
+
+
+
+
+
 import { Invoice } from './classes/invoice.js'
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
