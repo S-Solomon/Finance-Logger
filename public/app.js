6083d5ee1aa6fd5ee3c2dcef3e2929e9013d27b8
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-04-08 16:36:07 +0100

    rendering an html template

--- public/app.js
+++ public/app.js
@@ -1,50 +1,15 @@
-// interface isPerson {
-//     name: string;
-//     age: number;
-//     speak(a: string):void;
-//     spend(a: number):number;
-// }
-// const me: isPerson = {
-//     name: 'shun',
-//     age: 30,
-//     speak(text: string):void {
-//         console.log(text);
-//     },
-//     spend(amount: number):number {
-//         console.log(' I spent', amount);
-//         return amount
-//     }
-// }
-// const greetPerson = (person: isPerson) => {
-//     console.log('hello', person.name)
-// }
-// greetPerson(me)
-// let someone: isPerson;
-// console.log(me);
 import { Invoice } from './classes/invoice.js';
+import { ListTemplate } from './classes/ListTemplates.js';
 import { payment } from './classes/payment.js';
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new payment('mario', 'plumbing work', 200);
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs)
-// const invOne = new Invoice('mario', 'work on the mario website', 250);
-// const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-// let invoices: Invoice[] = [];
-// invoices.push(invOne)
-// invoices.push(invTwo)
-// invoices.forEach(inv => {
-//     console.log(inv.client, inv.amount, inv.format())
-// })
 const form = document.querySelector('.new-item-form');
 // inputs
 const type = document.querySelector('#type');
 const toFrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -54,5 +19,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new payment(toFrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,63 +1,7 @@
-// interface isPerson {
-//     name: string;
-//     age: number;
-
-//     speak(a: string):void;
-//     spend(a: number):number;
-// }
-
-// const me: isPerson = {
-//     name: 'shun',
-//     age: 30,
-//     speak(text: string):void {
-//         console.log(text);
-//     },
-//     spend(amount: number):number {
-//         console.log(' I spent', amount);
-//         return amount
-//     }
-// }
-
-// const greetPerson = (person: isPerson) => {
-//     console.log('hello', person.name)
-// }
-
-// greetPerson(me)
-
-// let someone: isPerson;
-
-
-// console.log(me);
-
-
-
-
 import { Invoice } from './classes/invoice.js'
+import { ListTemplate } from './classes/ListTemplates.js';
 import { payment } from './classes/payment.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
-
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new payment('mario', 'plumbing work', 200);
-
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// console.log(docs)
-
-// const invOne = new Invoice('mario', 'work on the mario website', 250);
-// const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-
-// let invoices: Invoice[] = [];
-// invoices.push(invOne)
-// invoices.push(invTwo)
-
-// invoices.forEach(inv => {
-//     console.log(inv.client, inv.amount, inv.format())
-// })
 
 
 
@@ -69,6 +13,10 @@ const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
@@ -79,5 +27,6 @@ form.addEventListener('submit', (e: Event) => {
         doc = new payment(toFrom.value, details.value, amount.valueAsNumber)
     }
 
-    console.log(doc);
+    list.render(doc, type.value, 'end');
+
 })
