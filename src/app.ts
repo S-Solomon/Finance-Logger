f7794106d0c12537444a866591d9f3cb7daa2dc9
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-04-08 14:53:10 +0100

    modules

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-
+import { Invoice } from './classes/invoice.js'
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
@@ -10,11 +10,6 @@ invoices.push(invTwo)
 invoices.forEach(inv => {
     console.log(inv.client, inv.amount, inv.format())
 })
-
-
-
-
-
 
 
 
